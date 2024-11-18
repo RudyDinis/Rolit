f8f6cc00f48298263a9469c33e9698d2b9f5e8a4
RudyDinis <60667200+RudyDinis@users.noreply.github.com>
2024-11-18 12:11:50 +0100

    Update pions.py

--- fonctions/pions.py
+++ fonctions/pions.py
@@ -55,28 +55,34 @@ def remplissage(plateau, indice, ordre, points):
 
 
 def place_pion(x_clic, y_clic, plateau, ordre, points):
-    for indice, cercle_info in enumerate(plateau):
-        x_centre, y_centre = cercle_info["centre"]  # récupère les centre x, y depuis la variable plateau
-        rayon = cercle_info["rayon"]  # récupère le rayon depuis la variable plateau
+    for indice_ligne, ligne in enumerate(plateau):
+        for indice, cercle_info in enumerate(ligne):
 
-        distance = ((x_clic - x_centre) ** 2 + (y_clic - y_centre) ** 2) ** 0.5  # Formule distance euclidienne
+            x_centre, y_centre = cercle_info["centre"]  # récupère les centre x, y depuis la variable plateau
+            rayon = cercle_info["rayon"]  # récupère le rayon depuis la variable plateau
 
-        if distance <= rayon:
-            if (list(ordre[0].keys())[0] != cercle_info["etat"]):  # Si le joueur clique sur une case qui ne lui appartient pas:
-                cercle(x_centre,y_centre,rayon,remplissage=list(ordre[0].values())[0],tag=[x_centre, y_centre, rayon],)
+            distance = ((x_clic - x_centre) ** 2 + (y_clic - y_centre) ** 2) ** 0.5  # Formule distance euclidienne
 
-                # Modification des points
-                points[list(ordre[0].keys())[0]] += 1
-                if cercle_info["etat"] != None:
-                    points[cercle_info["etat"]] -= 1
+            if distance <= rayon:
+                if (list(ordre[0].keys())[0] != cercle_info["etat"]):  # Si le joueur clique sur une case qui ne lui appartient pas:
+                    cercle(x_centre,y_centre,rayon,remplissage=list(ordre[0].values())[0],tag=[x_centre, y_centre, rayon],)
 
-                efface("points")
-                texte(900, 150, "Rouge : " + str(points["Rouge"]), tag="points")
-                texte(900, 250, "Jaune : " + str(points["Jaune"]), tag="points")
-                texte(900, 350, "Vert : " + str(points["Vert"]), tag="points")
-                texte(900, 450, "Bleu : " + str(points["Bleu"]), tag="points")
+                    # Modification des points
+                    points[list(ordre[0].keys())[0]] += 1
+                    if cercle_info["etat"] != None:
+                        points[cercle_info["etat"]] -= 1
 
-                # change l'etat de notre case
-                cercle_info["etat"] = list(ordre[0].keys())[0]
-                break
-    return remplissage(plateau, indice, ordre, points)
+                    efface("points")
+                    texte(900, 150, "Rouge : " + str(points["Rouge"]), tag="points")
+                    texte(900, 250, "Jaune : " + str(points["Jaune"]), tag="points")
+                    texte(900, 350, "Vert : " + str(points["Vert"]), tag="points")
+                    texte(900, 450, "Bleu : " + str(points["Bleu"]), tag="points")
+
+                    # change l'etat de notre case
+                    cercle_info["etat"] = list(ordre[0].keys())[0]
+
+                    ordre.append(ordre.pop(0))
+                    efface("tour_joueur")
+                    texte(90,10,"au tour du joueur : " + list(ordre[0].keys())[0],couleur="black",taille=40,tag="tour_joueur")
+                    break
+    return plateau, ordre, points
